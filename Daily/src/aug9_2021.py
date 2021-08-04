6a0a68ce1aeca84d16f94fa8687e16921e473f42
PanTaixi <2330610648@qq.com>
2021-08-04 19:08:21 +0800

    Move code around

--- Daily/src/aug9_2021.py
+++ Daily/src/aug9_2021.py
@@ -15,31 +15,6 @@ def triangle_number(nums):
     return count
 
 
-# returns list of r-list-tuples
-# e.g. [[1,2,3],[1,2,4],[1,3,4],[2,3,4]]
-def list_choose_r(ls, r):
-    assert r > 0
-
-    # choose r elements from the list
-    if len(ls) < r:
-        return []
-    elif len(ls) == r:
-        return [copy.deepcopy(ls)]
-
-    if r == 1:
-        return [[x] for x in ls]
-    else:
-        result = []
-        for i in range(0, len(ls) - r + 1):
-            x = ls[i]
-            rest = ls[(i + 1):]
-            rest_choose = list_choose_r(rest, (r - 1))
-            for_this_x = [([x] + rc) for rc in rest_choose if rc]
-            result += for_this_x
-
-        return result
-
-
 def list_choose_r_fast(ls, r):
     table = [[[] for i in range(r)] for j in range(len(ls))]
     return list_choose_r_dp(ls, r, 0, table)
@@ -82,3 +57,28 @@ def list_choose_r_dp(ls, r, start, table):
 class Solution:
     def triangleNumber(self, nums: List[int]) -> int:
         return triangle_number(nums)
+
+
+# returns list of r-list-tuples
+# e.g. [[1,2,3],[1,2,4],[1,3,4],[2,3,4]]
+def list_choose_r(ls, r):
+    assert r > 0
+
+    # choose r elements from the list
+    if len(ls) < r:
+        return []
+    elif len(ls) == r:
+        return [copy.deepcopy(ls)]
+
+    if r == 1:
+        return [[x] for x in ls]
+    else:
+        result = []
+        for i in range(0, len(ls) - r + 1):
+            x = ls[i]
+            rest = ls[(i + 1):]
+            rest_choose = list_choose_r(rest, (r - 1))
+            for_this_x = [([x] + rc) for rc in rest_choose if rc]
+            result += for_this_x
+
+        return result

c503db685211ba4150340072c12265a87bbe531b
PanTaixi <2330610648@qq.com>
2021-08-09 14:06:00 +0800

    added test cases but over time

--- Daily/test/test_aug9_2021.py
+++ Daily/test/test_aug9_2021.py
@@ -20,3 +20,22 @@ class TestGenerateUglySeq(unittest.TestCase):
         result = self.seq_generator(n, sorted_primes)
         answer = [1, 2, 4, 7, 8, 13, 14, 16, 19, 26, 28, 32]
         self.assertListEqual(answer, result)
+
+
+class TestOverall(unittest.TestCase):
+    def setUp(self) -> None:
+        self.test_subject = aug9_2021.nth_super_ugly_number
+
+    def test_small_case(self):
+        n = 1
+        primes = [2, 3, 5]
+        result = self.test_subject(n, primes)
+        answer = 1
+        self.assertEqual(answer, result)
+
+    def test_medium_case(self):
+        n = 12
+        primes = [2, 7, 13, 19]
+        result = self.test_subject(n, primes)
+        answer = 32
+        self.assertEqual(answer, result)

732ea87ee70bc20769a36172dbc98001845ff643
PanTaixi <2330610648@qq.com>
2021-08-10 10:35:19 +0800

    use heapq to optimize search
    now passes

--- Daily/src/aug9_2021.py
+++ Daily/src/aug9_2021.py
@@ -1,8 +1,71 @@
 import math
 from typing import List
+import heapq
 
 
 # No 313
+
+class PotentialCombination:
+    def __init__(self, result_position, sorted_primes, super_ugly_numbers):
+        self.result_position = result_position
+        self.sorted_primes = sorted_primes
+        self.super_ugly_numbers = super_ugly_numbers
+
+        self.n_sorted_primes = len(self.sorted_primes)
+        self.sorted_primes_index = 0
+        self.product = self.calculate_product()
+
+    def next_comb(self):
+        self.sorted_primes_index += 1
+        if self.is_in_range():
+            self.product = self.calculate_product()
+
+    def calculate_product(self):
+        return self.super_ugly_numbers[self.result_position] * self.sorted_primes[self.sorted_primes_index]
+
+    def is_in_range(self):
+        return self.sorted_primes_index < self.n_sorted_primes
+
+    def __lt__(self, other):
+        return self.product < other.product
+
+
+def generate_super_ugly_sequence_heap(n, sorted_primes):
+    # n_sorted_primes = len(sorted_primes)
+    result = [0 for _ in range(n)]
+    comb_heap = []  # entry: PotentialCombination
+
+    result[0] = 1
+
+    initial_comb = PotentialCombination(0, sorted_primes, result)
+
+    heapq.heappush(comb_heap, initial_comb)
+
+    for current_result_position in range(1, n):
+
+        # Pops from the heap the smallest ones
+        smallest_product = comb_heap[0].product
+        all_smallest_combs = []
+        while comb_heap and comb_heap[0].product == smallest_product:
+            small_comb = heapq.heappop(comb_heap)
+            all_smallest_combs.append(small_comb)
+
+        # Update the result
+        result[current_result_position] = smallest_product
+
+        # Update the comb_heap
+        # The used ones
+        for comb in all_smallest_combs:
+            comb.next_comb()
+            if comb.is_in_range():
+                heapq.heappush(comb_heap, comb)
+
+        # The new one
+        new_comb = PotentialCombination(current_result_position, sorted_primes, result)
+        heapq.heappush(comb_heap, new_comb)
+
+    return result
+
 
 # List_A : sorted primes
 # List_B : super ugly sequence
@@ -15,8 +78,7 @@ def generate_super_ugly_sequence(n, sorted_primes):
     result[current_result_position] = 1
     current_comb_table[0] = (0, sorted_primes[0])
 
-    while current_result_position < n - 1:
-        current_result_position += 1
+    for current_result_position in range(1, n):
 
         # Find which one to be appended (the smallest one)
         smallest_comb_indices, smallest_comb_product = find_smallest_combs(current_comb_table, current_result_position,
@@ -63,7 +125,7 @@ def find_smallest_combs(current_comb_table, current_result_position, n_sorted_pr
 
 
 def nth_super_ugly_number(n, primes):
-    super_ugly_sequence = generate_super_ugly_sequence(n, sorted(primes))
+    super_ugly_sequence = generate_super_ugly_sequence_heap(n, sorted(primes))
     return super_ugly_sequence[n - 1]
 
 
--- Daily/test/test_aug9_2021.py
+++ Daily/test/test_aug9_2021.py
@@ -1,3 +1,4 @@
+import heapq
 import unittest
 
 from Daily.src import aug9_2021
@@ -5,7 +6,7 @@ from Daily.src import aug9_2021
 
 class TestGenerateUglySeq(unittest.TestCase):
     def setUp(self) -> None:
-        self.seq_generator = aug9_2021.generate_super_ugly_sequence
+        self.seq_generator = aug9_2021.generate_super_ugly_sequence_heap
 
     def test_generate_short_seq(self):
         n = 3
@@ -41,9 +42,39 @@ class TestOverall(unittest.TestCase):
         self.assertEqual(answer, result)
 
     def test_large_case(self):
-        n = 10000
+        n = 100000
         primes = [7, 19, 29, 37, 41, 47, 53, 59, 61, 79, 83, 89, 101, 103, 109, 127, 131, 137, 139, 157, 167, 179, 181,
                   199, 211,
                   229, 233, 239, 241, 251]
         result = self.test_subject(n, primes)
-        print(result)
+        answer = 1092889481
+        self.assertEqual(answer, result)
+
+
+class TestPotentialCombClass(unittest.TestCase):
+    def setUp(self) -> None:
+        self.sorted_primes = [2, 5]
+
+    def test_less_than(self):
+        result = [1, 2]
+        comb_1 = aug9_2021.PotentialCombination(0, self.sorted_primes, result)
+        comb_2 = aug9_2021.PotentialCombination(1, self.sorted_primes, result)
+        self.assertTrue(comb_1 < comb_2)
+
+    def test_less_than_case_2(self):
+        result = [1, 2]
+        comb_1 = aug9_2021.PotentialCombination(0, self.sorted_primes, result)
+        comb_2 = aug9_2021.PotentialCombination(1, self.sorted_primes, result)
+        comb_1.next_comb()
+        self.assertLess(comb_2, comb_1)
+
+    def test_works_with_heapq(self):
+        comb_heap = []
+        result = [1, 2]
+        comb_1 = aug9_2021.PotentialCombination(0, self.sorted_primes, result)
+        comb_2 = aug9_2021.PotentialCombination(1, self.sorted_primes, result)
+        comb_1.next_comb()
+
+        heapq.heappush(comb_heap, comb_1)
+        heapq.heappush(comb_heap, comb_2)
+        self.assertEqual(4, comb_heap[0].product)

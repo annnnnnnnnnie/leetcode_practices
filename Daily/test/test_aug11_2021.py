264c5aedbad8cc1a517be35a273292c5ea94caf2
PanTaixi <2330610648@qq.com>
2021-08-12 22:05:13 +0800

    still overtime

--- Daily/test/test_aug11_2021.py
+++ Daily/test/test_aug11_2021.py
@@ -39,6 +39,15 @@ class TestOverall(unittest.TestCase):
         answer = sum(math.comb(n_ss, i) for i in range(3, n_ss + 1))
         self.perform_simple_test(nums, answer)
 
+    def test_long_case_2(self):
+        nums = [79, 20, 64, 28, 67, 81, 60, 58, 97, 85, 92, 96, 82, 89, 46, 50, 15, 2, 36, 44, 54, 2, 90, 37, 7, 79, 26,
+                40, 34, 67, 64, 28, 60, 89, 46, 31, 9, 95, 43, 19, 47, 64, 48, 95, 80, 31, 47, 19, 72, 99, 28, 46, 13,
+                9, 64, 4, 68, 74, 50, 28, 69, 94, 93, 3, 80, 78, 23, 80, 43, 49, 77, 18, 68, 28, 13, 61, 34, 44, 80, 70,
+                55, 85, 0, 37, 93, 40, 47, 47, 45, 23, 26, 74, 45, 67, 34, 20, 33, 71, 48, 96]
+        n_ss = len(nums)
+        answer = sum(math.comb(n_ss, i) for i in range(3, n_ss + 1))
+        self.perform_simple_test(nums, answer)
+
 
 class TestForwardDistanceMatrixGeneration(unittest.TestCase):
     def setUp(self) -> None:
